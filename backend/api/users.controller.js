752f01ef7d8eea8233b2995ff2cdef2fca65f6fc
BENJIGAO <bhgao20014@gmail.com>
2022-02-19 13:00:55 -0500

    setup base working server

---
+++ backend/api/users.controller.js
@@ -0,0 +1,5 @@
+export default class UsersController {
+  static async apiGetUsers(req, res, next) {
+    res.json("This is a test")
+  }
+}
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import router from "./api/g_aim.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/g_aim", router)
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+
+export default app
+
