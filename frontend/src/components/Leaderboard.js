f2e51cb238be91fa72a3b225efcc060b1acfea4d
ericlon <ericlinjh@hotmail.com>
2022-02-19 23:10:05 -0500

    Unabstracted Navbar, made it actually show what's active

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar'
 import clouds from '../images/parallax-homepage/clouds.png'
 import { TableBody, TableCell, TableContainer, TableHead, TableRow, Paper}from '@mui/material'
+import seagulls from '../sound/seagulls.mp3'
+import Sound from 'react-sound'
+import { Link } from 'react-router-dom'
+import logo from '../images/parallax-homepage/gaimLogo.png'
 
 export default function TempLeaderboard() {
     const scores = [
@@ -20,7 +23,38 @@ export default function TempLeaderboard() {
         justifyContent: "center",
         alignItems: "center"
       }}>
-          <Navbar />
+          <header id="header">
+            <Sound
+                url={seagulls}
+                playStatus={Sound.status.PLAYING}
+                playFromPosition={100}
+                volume={5}
+            >
+            </Sound>
+            <a href='/'><img src={logo} alt='logo' id="logo"></img></a> 
+            <ul>
+                <li>
+                    <Link to="/" className='header-button'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/leaderboard" className='active header-button'>                        
+                        Leaderboard
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/login" className='header-button'>
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/play" className='header-button'>
+                        Play
+                    </Link>
+                </li>
+            </ul>
+          </header>
           <TableContainer component={Paper} sx={{backgroundColor: "#FDF7C2", width:"auto", height: "auto"}}>
             <TableHead>
               <TableRow>
