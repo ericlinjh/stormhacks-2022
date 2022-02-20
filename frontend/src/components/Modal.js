3900bbeaae5419100623ca5123207847d5ed7f4e
BENJIGAO <bhgao20014@gmail.com>
2022-02-19 21:57:30 -0500

    add exit modal

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../Modal.css'
 
-export default function Modal({closeModal}) {
+export default function Popup({closeModal}) {
     return (
         <div className="modalBackground">
             <div className="modalContainer">
-                <div className="titelCloseBtn">
+                <div className="titleCloseBtn">
                 <button onClick={()=> closeModal(false)}>X</button>
                 </div>
                 <div className='title'>
